dc2aa61816091ea9193ac33ec885d91f8383b7b2
GianniMejia <101169251+GianniMejia@users.noreply.github.com>
2022-08-21 01:09:17 -0700

    updated index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 import express from "express";
 import apiRoutes from "./api/index.js";
 
+export const router = express.Router();
+
+import { router as userRoutes } from "./user-routes.js";
+import { router as thoughtRoutes } from "./thought-routes.js";
+
+router.use("/users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
+
 const router = express.Router();
 router.use("/api", apiRoutes);
 
